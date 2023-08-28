bf837453cecf01b37656f64d1ac4349ff2e11ee6
htonioni <htonioni@outlook.com>
2023-08-28 10:49:53 -0300

    Added class #11 files

---
+++ Javascript/Modulo_D/ex008_9.js
@@ -0,0 +1,20 @@
+var vel = 71
+console.log(`A velocidade do seu carro era de: ${vel}km/h`);
+
+if (vel>70){
+    console.log(`Voce ultrapassou a velocidade permitida!
+Multado.`);     
+}
+
+console.log('Dirija sempre com cinto de seguranca');
+
+//ex009
+console.log();
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`);
+
+if (pais === 'Brasil') {
+    console.log(`Voce eh brasiliero`);
+} else {
+    console.log(`Voce eh estrangeiro`);
+}
