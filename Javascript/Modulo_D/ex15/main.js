6dca7dd6ff5b5f36efb373f6cb09fd5d915b0d2a
htonioni <htonioni@outlook.com>
2023-09-13 10:40:25 -0300

    Update main files

--- Javascript/Modulo_D/ex15/main.js
+++ Javascript/Modulo_D/ex15/main.js
@@ -1,14 +1,44 @@
-//<img src="" alt="">
-let anoAtual = new Date().getFullYear()
-let fano = document.getElementById("txtano").value
-const radioM = document.getElementById("masc")
-const divImg = document.querySelector('div#img')
 
 
 
 function verificar() {
-    let idade = anoAtual - fano
-    if (fano.length == 0 || fano > anoAtual){
+    var ano = new Date().getFullYear()
+    var formAno = document.getElementById("txtano")
+    const divRes = document.querySelector('div#res')
+    if (formAno.value > ano || formAno.value == 0 || formAno.value < (ano - 150)){
         alert("[ERRO] Verifique os dados e tente novamente")
-    } 
+    } else {
+        var formSex = document.getElementsByName('radsex')
+        var idade = ano - Number(formAno.value)
+        var genero = ''
+        //Criando tag img
+        var tagImg = document.createElement('img')
+        //Adicionando a tag o id='foto'
+        tagImg.setAttribute('id', 'foto')
+        if (formSex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                tagImg.setAttribute('src', 'img/bM.jpg')
+            } else if (idade <= 18){
+                tagImg.setAttribute('src', 'img/jM.jpg')
+            } else if (idade <= 51){
+                tagImg.setAttribute('src', 'img/aM.jpg')
+            } else {
+                tagImg.setAttribute('src', 'img/oM.jpg')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //bm
+            } else if (idade <= 18){
+                // jm
+            } else if (idade <= 51){
+                //aM
+            } else {
+                // oM
+            }
+        }
+        divRes.innerHTML = `Foi encontrado um ${genero} de ${idade} anos`
+        divRes.appendChild(tagImg) 
+    }
 }
