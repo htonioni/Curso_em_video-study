f4ee4875e8217f5aa8f719a3be83569596e48c57
htonioni <htonioni@outlook.com>
2023-09-25 20:55:13 -0300

    Added error treatment

--- Javascript/Modulo_E/ex17/main.js
+++ Javascript/Modulo_E/ex17/main.js
@@ -1,11 +1,17 @@
 function tabuada() {
-    let numero = Number(document.getElementById("txtn").value)
+    let numero = document.getElementById("txtn").value
     let lista = document.getElementById("lista")
-    
-    for (let i = 1; i <= 10; i++){
-        if (i == 1){
-            lista.innerHTML = `<option>Segue a tabua do ${numero}.....</option>`
+    if (numero.length == 0) {
+        alert("[ERRO] Valor invalido!")
+    } else {
+        let num = Number(numero)
+        for (let i = 1; i <= 10; i++){
+            if (i == 1){
+                lista.innerHTML = `<option>Segue a tabua do ${numero}.....</option>`
+                // let item = document.createElement('option')
+                // lista.appendChild(item)
+            }
+            lista.innerHTML += `<option>${numero} x ${i} = ${numero * i}</option>`
         }
-        lista.innerHTML += `<option>${numero} x ${i} = ${numero * i}</option>`
     }
 }
